f5460bfa7dfc520dc0d6dd80a909d2253e789c5f
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-15 16:22:22 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
   const Login = async () => {
     setLoading(true);
     if (!email || !password){ setLoading(false); return notify("Enter credentials");}
-    try { const response = await fetch("https://driveapi-tz5l.vercel.app/api/v1/users/login", {
+    try { const response = await fetch("https://driveapi-seven.vercel.app/api/v1/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
