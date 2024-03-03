11c96028708111bf2371f965c769707af99c37a6
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-03-03 20:13:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const currentUrl = window.location.href;
    async function Verification() {
  try {
 
-      const auth = await fetch("https://driveapi-seven.vercel.app/api/v1/users/", {
+      const auth = await fetch("https://driveapi.onrender.com/api/v1/users/", {
         method: 'post',
         headers: {
           Accept: "application/json",
